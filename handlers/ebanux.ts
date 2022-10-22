02168b86880eaca962cec6923b3b041523accba5
yosle <yosle007@gmail.com>
2022-10-22 11:35:48 -0400

    changes on ebanux handler

--- handlers/ebanux.ts
+++ handlers/ebanux.ts
@@ -1,16 +1,10 @@
 import axios from 'axios'
-import { CustomError } from "../utils";
-import { Auth, Amplify } from 'aws-amplify';
-import awsExports from "../libs/aws/aws-exports";
 
+import { Auth } from 'aws-amplify';
 
-const SERVER_URL = process.env.DEVELOPMENT_MODE ? 'https://ebanux.com' : 'https://dev.ebanux.link'
-Amplify.configure(awsExports);
-const cognitoSession = await Auth.currentSession();
-const cognitoToken = cognitoSession.getAccessToken().getJwtToken(
+const SERVER_URL = process.env.DEVELOPMENT_MODE ? 'https://dev.ebanux.link' : 'https://ebanux.link'
+// const cognitoSession = await Auth.currentSession();
 
-
-)
 const APIv1 = axios.create({
     baseURL: `${SERVER_URL}/api/v1`
 });
@@ -18,38 +12,18 @@ const APIv1 = axios.create({
 
 //Temporarely use of APIv1 and APIv2 until is fully migrated
 export const APIv2 = axios.create({
-    baseURL: `${SERVER_URL}/api/v2.0`
+    baseURL: `${SERVER_URL}/api/v2.0`,
+
 });
 
-/**
- * 
- * @param userId Cognito User Id
- * @param unitAmountUSD 
- * @param quantity 
- */
-export const createEbanuxDonationCoffiePrice = async (
-    userId: string,
-    unitAmountUSD: number,
-    jwtToken: string
-) => {
-    console.log('executing handler', userId, unitAmountUSD)
-    //handle errors   
-
-    const response = await APIv1.post('/donation',
-        {//data
-            cognitoUserId: userId,
-            amount: unitAmountUSD
-        }, {
+export const createEbanuxDonationPrice = async (userId: string, token: string, unitAmountUSD: number) => {
+    const result = await APIv1.post('/donation', {
+        amount: unitAmountUSD,
+        cognitoUserId: userId
+    }, {
         headers: {
-            'Authorization': `Bearer ${cognitoToken}`
+            'Authorization': `Bearer ${token}`
         }
-    })
-    return response;
-
-
-}
-
-export const updateCoffieDonationPrice = async () => {
-
-
+    });
+    return result;
 }
