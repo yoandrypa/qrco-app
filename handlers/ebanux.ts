ca625be5f13f2811ac63fdc966a3e89d48a39b30
yosle <yosle007@gmail.com>
2022-10-25 19:26:49 -0400

    API handler

--- handlers/ebanux.ts
+++ handlers/ebanux.ts
@@ -1,14 +1,10 @@
 import axios from 'axios'
-
-import { Auth } from 'aws-amplify';
-
-const SERVER_URL = process.env.DEVELOPMENT_MODE ? 'https://dev.ebanux.link' : 'https://ebanux.link'
-// const cognitoSession = await Auth.currentSession();
+const SERVER_URL = process.env.NEXT_PUBLIC_DEVELOPMENT_MODE ? 'https://dev.ebanux.link' : 'https://ebanux.link'
+import { EbanuxDonationPriceData } from '../components/qr/types/types';
 
 const APIv1 = axios.create({
     baseURL: `${SERVER_URL}/api/v1`
 });
-
 
 //Temporarely use of APIv1 and APIv2 until is fully migrated
 export const APIv2 = axios.create({
@@ -16,9 +12,9 @@ export const APIv2 = axios.create({
 
 });
 
-export const createEbanuxDonationPrice = async (userId: string, token: string, unitAmountUSD: number) => {
+export const createEbanuxDonationPrice = async (userId: string, token: string, data: EbanuxDonationPriceData) => {
     const result = await APIv1.post('/donation', {
-        amount: unitAmountUSD,
+        amount: data.unitAmountUSD * 100,
         cognitoUserId: userId
     }, {
         headers: {
@@ -26,4 +22,8 @@ export const createEbanuxDonationPrice = async (userId: string, token: string, u
         }
     });
     return result;
+}
+
+async function updateEbanuxDonationPrice(userId: string, token: string, priceId: string, data: EbanuxDonationPriceData) {
+    //TODO
 }
