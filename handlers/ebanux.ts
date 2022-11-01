51708507df0f9192e71e0f42414b047f66dcc89c
yosle <yosle007@gmail.com>
2022-11-01 08:00:09 -0400

    create update EbanuxDonation Price method

--- handlers/ebanux.ts
+++ handlers/ebanux.ts
@@ -25,5 +25,13 @@ export const createEbanuxDonationPrice = async (userId: string, token: string, d
 }
 
 async function updateEbanuxDonationPrice(userId: string, token: string, priceId: string, data: EbanuxDonationPriceData) {
-    //TODO
+    const result = await APIv1.put('/donation', {
+        amount: data.unitAmountUSD,
+        cognitoUserId: userId
+    }, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    return result;
 }
