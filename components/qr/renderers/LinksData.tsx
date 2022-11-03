ba1c59178f45df85f4c2401a1ef913cecbf2b6f2
soma <soma@velosimo.com>
2022-11-03 12:42:14 -0400

    - removing not needed file and dependencies - adding the drag and drop capabilities

--- components/qr/renderers/LinksData.tsx
+++ components/qr/renderers/LinksData.tsx
@@ -22,6 +22,8 @@ import VerticalAlignCenterIcon from '@mui/icons-material/VerticalAlignCenter';
 import VerticalAlignBottomIcon from '@mui/icons-material/VerticalAlignBottom';
 import VerticalAlignTopIcon from '@mui/icons-material/VerticalAlignTop';
 
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import Common from '../helperComponents/Common';
 import Topics from "./helpers/Topics";
 import {isValidUrl} from "../../../utils";
@@ -40,6 +42,12 @@ interface LinksDataProps {
   handleValues: Function;
   setIsWrong: (isWrong: boolean) => void;
 }
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  userSelect: "none",
+  background: isDragging ? "#ebe8e1" : "none",
+  ...draggableStyle
+});
 
 export default function LinksData({data, setData, handleValues, setIsWrong}: LinksDataProps) {
   const [expander, setExpander] = useState<string | null>(null);
@@ -85,6 +93,23 @@ export default function LinksData({data, setData, handleValues, setIsWrong}: Lin
     return 'In the middle of the URLs';
   }, [linksAmount, amount]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const onDragEnd = (result: any) => {
+    if (!result?.destination) {
+      return null;
+    }
+
+    setData((prev: DataType) => {
+      const tempo = {...prev};
+
+      const newLinks = Array.from(tempo.links || []);
+      const [removed] = newLinks.splice(result.source.index, 1);
+      newLinks.splice(result.destination.index, 0, removed);
+
+      tempo.links = newLinks;
+      return tempo;
+    });
+  }
+
   useEffect(() => {
     let isWrong = false;
     if (!data?.title?.trim().length || data?.links?.some((x: LinkType) => (!x.label.trim().length ||
@@ -114,45 +139,64 @@ export default function LinksData({data, setData, handleValues, setIsWrong}: Lin
         </Grid>
       </Grid>
       <Topics message="Links" top="3px" secMessage={data.links && `(${pluralize('link', data.links.length, true)})`}/>
-      <TableContainer sx={{mt: '-8px'}}>
-        <Table size="small">
-          <TableBody>
-            {data.links?.length && data.links.map((x: LinkType, index: number) => (
-              <TableRow sx={{p: 0, width: '100%'}} key={`trow${index}`}>
-                <TableCell sx={{p: 0, pr: 1, width: '50%', borderBottom: 'none'}}>
-                  <RenderProposalsTextFields
-                    required
-                    options={['My website', 'My youtube channel', 'My blog', 'My portfolio', 'My podcast', 'My store']}
-                    placeholder="Label here"
-                    value={x.label}
-                    handleValues={handleChangeValue('label', index)}
-                  />
-                </TableCell>
-                <TableCell sx={{p: 0, width: '50%', borderBottom: 'none'}}>
-                  <RenderTextFields
-                    required
-                    placeholder="Link here"
-                    value={x.link}
-                    handleValues={handleChangeValue('link', index)}
-                    isError={x.link.trim().length > 0 && !isValidUrl(x.link)}
-                  />
-                </TableCell>
-                <TableCell sx={{p: 0, borderBottom: 'none'}} align="right">
-                  {index === 0 ? (
-                    <Tooltip title={'Add a link'}>
-                      <IconButton onClick={add}><AddBoxIcon color="primary"/></IconButton>
-                    </Tooltip>
-                  ) : (
-                    <Tooltip title={'Remove link'}>
-                      <IconButton onClick={remove(index)}><DeleteIcon color="error"/></IconButton>
-                    </Tooltip>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided: any) => (
+            <TableContainer sx={{mt: '-8px'}}>
+              <Table size="small">
+                <TableBody {...provided.droppableProps} ref={provided.innerRef}>
+                  {data.links?.length && data.links.map((x: LinkType, index: number) => {
+                    const itemId = `item${index}`;
+                    return ( // @ts-ignore
+                      <Draggable key={itemId} draggableId={itemId} index={index} isDragDisabled={data.links.length === 1}>
+                        {(provided: any, snapshot: any) => (
+                          <TableRow
+                            sx={{p: 0, width: '100%'}}
+                            key={`trow${index}`}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}>
+                            <TableCell sx={{p: 0, pr: 1, width: '50%', borderBottom: 'none'}}>
+                              <RenderProposalsTextFields
+                                required
+                                options={['My website', 'My youtube channel', 'My blog', 'My portfolio', 'My podcast', 'My store']}
+                                placeholder="Label here"
+                                value={x.label}
+                                handleValues={handleChangeValue('label', index)}
+                              />
+                            </TableCell>
+                            <TableCell sx={{p: 0, width: '50%', borderBottom: 'none'}}>
+                              <RenderTextFields
+                                required
+                                placeholder="Link here"
+                                value={x.link}
+                                handleValues={handleChangeValue('link', index)}
+                                isError={x.link.trim().length > 0 && !isValidUrl(x.link)}
+                              />
+                            </TableCell>
+                            <TableCell sx={{p: 0, borderBottom: 'none'}} align="right">
+                              {index === 0 ? (
+                                <Tooltip title={'Add a link'}>
+                                  <IconButton onClick={add}><AddBoxIcon color="primary"/></IconButton>
+                                </Tooltip>
+                              ) : (
+                                <Tooltip title={'Remove link'}>
+                                  <IconButton onClick={remove(index)}><DeleteIcon color="error"/></IconButton>
+                                </Tooltip>
+                              )}
+                            </TableCell>
+                          </TableRow>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </Droppable>
+      </DragDropContext>
       <Grid item xs={12}>
         <Divider sx={{my: 1}}/>
         <Paper elevation={2} sx={{p: 1, mt: 1}}>
