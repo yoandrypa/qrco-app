644efc74ac5e917c0a986ce9f4f4d850e0034cde
soma <soma@velosimo.com>
2022-11-09 12:25:40 -0500

    fixing slint

--- components/qr/renderers/validator.ts
+++ components/qr/renderers/validator.ts
@@ -5,7 +5,8 @@ export default function socialsAreValid(data: DataType) {
   let result = true;
   if (data?.socials?.length) {
     data.socials.every((x: SocialNetworksType) => {
-      if ((x.value !== undefined && x.value.trim().length === 0) || (x.network === 'whatsapp' && !PHONE.test(x.value || ''))) {
+      if ((x.value !== undefined && x.value.trim().length === 0) ||
+        (x.network === 'whatsapp' && !PHONE.test(x.value || ''))) {
         result = false;
         return false;
       }
