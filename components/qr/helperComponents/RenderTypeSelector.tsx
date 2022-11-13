21fdfc46190da2404776a0588d0e4ebdaf9128b0
yosle <yosle007@gmail.com>
2022-11-13 09:12:32 -0500

    retrieve changes

--- components/qr/helperComponents/RenderTypeSelector.tsx
+++ components/qr/helperComponents/RenderTypeSelector.tsx
@@ -75,7 +75,7 @@ const RenderTypeSelector = ({ selected, handleSelect }: RenderTypeSelectorProps)
           {renderTypeSelector("link", "Link-in-Bio", "Share your own links, including social info", true)}
           {renderTypeSelector("coupon", "Coupon", "Share a coupon", true)}
           {renderTypeSelector("donations", "Donations", "Get donations from your supporters worldwide", true)}
-          {process.env.REACT_NODE_ENV === 'develop' && renderTypeSelector("fundme", "Fund Me", "Start your own charity or fundraising campaign.", true)}
+          {process.env.REACT_NODE_ENV === 'develop' && renderTypeSelector("fundme", "Fund Me", "Start your own charity or fundraising campaign", true)}
         </>)
       }
       {renderTypeSelector("twitter", "Twitter", "Post a tweet", true)}
