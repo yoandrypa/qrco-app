39fdea6c105b65a4f8ec9ad15ed4ba94076b1e68
soma <soma@velosimo.com>
2022-11-18 14:08:38 -0500

    updating ui

--- components/qr/helperComponents/TypeSelector.tsx
+++ components/qr/helperComponents/TypeSelector.tsx
@@ -1,19 +1,18 @@
 import {useState} from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
 import Popover from "@mui/material/Popover";
-import Paper from "@mui/material/Paper";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import IconButton from '@mui/material/IconButton';
-import {grey} from "@mui/material/colors";
+import grey from "@mui/material/colors/grey";
 import {alpha} from "@mui/material/styles";
 
 import RenderIcon from "./RenderIcon";
 import RenderIframe from "../../RenderIframe";
 import {NO_MICROSITE} from "../constants";
 import RenderCellPhoneShape from "./RenderCellPhoneShape";
+import {useTheme} from "@mui/system";
 
 interface TypeSelectorProps {
   handleSelect: (payload: string) => void;
@@ -27,6 +26,8 @@ interface TypeSelectorProps {
 
 const TypeSelector = ({ handleSelect, label, description, icon, selected, isDynamic, enabled = true}: TypeSelectorProps) => {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [over, setOver] = useState<boolean>(selected);
+  const theme = useTheme();
 
   const beforeHandle = (event: any) => {
     if (enabled && !['svg', 'BUTTON'].includes(event.target.tagName)) {
@@ -38,6 +39,10 @@ const TypeSelector = ({ handleSelect, label, description, icon, selected, isDyna
     setAnchorEl(event.currentTarget);
   };
 
+  const handleOver = (): void => {
+    setOver((prev: boolean) => !prev);
+  }
+
   return (
     <>
       <Box
@@ -47,13 +52,15 @@ const TypeSelector = ({ handleSelect, label, description, icon, selected, isDyna
           width: '100%',
           height: '130px',
           borderRadius: '5px',
-          border: theme => `solid 1px ${theme.palette.text.disabled}`,
+          border: `solid 1px ${grey[500]}`,
           backgroundColor: enabled ? '#fff' : grey[300],
           boxShadow: enabled && selected ? '0 0 5px 2px #286ED6' : 'none',
           '&:hover': enabled ? {
             boxShadow: !selected ? '0 0 3px 2px #849abb' : '0 0 3px 2px #286ED6',
           } : grey[100]
         }}
+        onMouseEnter={handleOver}
+        onMouseLeave={handleOver}
         onClick={beforeHandle}
       >
         {isDynamic && !NO_MICROSITE.includes(icon) && (
@@ -63,8 +70,8 @@ const TypeSelector = ({ handleSelect, label, description, icon, selected, isDyna
                 position: 'absolute',
                 right: '5px',
                 bottom: 0,
-                color: theme => alpha(theme.palette.primary.dark, 0.25),
-                '&:hover': {color: theme => theme.palette.primary.dark}
+                color: alpha(theme.palette.primary.dark, 0.25),
+                '&:hover': {color: theme.palette.primary.dark}
               }}
               onClick={handleClick}
               id={`example${icon}`}>
@@ -74,19 +81,19 @@ const TypeSelector = ({ handleSelect, label, description, icon, selected, isDyna
         )}
         <Box sx={{display: 'flex', p: 1}}>
           <Box sx={{mt: '3px'}}>
-            <RenderIcon icon={icon} enabled={enabled}/>
+            <RenderIcon icon={icon} enabled={enabled} color={!selected && !over ? grey[500] : undefined} />
           </Box>
           <Box sx={{display: 'flex', flexDirection: 'column', textAlign: 'left', ml: 1, width: '100%'}}>
             <Box sx={{display: 'flex', width: '100%', justifyContent: 'space-between'}}>
               <Typography sx={{
                 width: '100%',
                 fontWeight: 'bold',
-                color: !enabled ? theme => theme.palette.text.disabled : 'default'
+                color: !enabled ? grey[500] : (selected || over ? 'default' : grey[700])
               }} variant="h6">
                 {label}
               </Typography>
             </Box>
-            <Typography sx={{width: '100%', color: theme => theme.palette.text.disabled}}>
+            <Typography sx={{width: '100%', color: !enabled ? grey[500] : (selected || over ? grey[800] : grey[600])}}>
               {description}
             </Typography>
           </Box>
@@ -101,10 +108,10 @@ const TypeSelector = ({ handleSelect, label, description, icon, selected, isDyna
           anchorOrigin={{vertical: 'top', horizontal: 'center'}}
           transformOrigin={{vertical: 'bottom', horizontal: 'center'}}
           disableRestoreFocus
-          sx={{ '.MuiPopover-paper': { borderRadius: '30px' }, boxShadow: '10px 10px 5px lightblue', }}
+          sx={{ '.MuiPopover-paper': { borderRadius: '25px', boxShadow: '0px 7px 5px 0px rgb(0 0 0 / 50%)' } }}
         >
           <RenderCellPhoneShape onClose={() => setAnchorEl(null)}>
-            <RenderIframe src={`${process.env.REACT_MICROSITES_ROUTE}/sample/${icon}`} width={308} height={510}/>
+            <RenderIframe src={`${process.env.REACT_MICROSITES_ROUTE}/sample/${icon}`} width={291} height={520}/>
           </RenderCellPhoneShape>
         </Popover>
       )}
