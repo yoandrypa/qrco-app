232abaf153f21e057a754fa340ae096c75b9794b
yosle <yosle007@gmail.com>
2022-11-29 15:30:48 -0500

    reduced plan component

--- pages/plans/buy/[plan].tsx
+++ pages/plans/buy/[plan].tsx
@@ -1,13 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { get } from '../../../handlers/users'
-import Context from '../../../components/context/Context'
-import { handleFetchResponse } from '../../../handlers/helpers'
 import { GetServerSideProps } from 'next'
-import Loading from '../../../components/Loading'
-import Alert from '@mui/material/Alert'
-import Snackbar from '@mui/material/Snackbar'
-
+import BuyPlan from '../../../components/plans/BuyPlan'
+//Generic Imports
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
@@ -29,60 +25,8 @@ type Props = {
 Amplify.configure(awsExports);
 
 const PlanType = (props: Props) => {
-    const [error, setError] = useState<string | null>(null)
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const router = useRouter();
-    //@ts-ignore
-    const { userInfo } = useContext(Context)
-
-    useEffect(() => {
-        const { plan } = router.query
-        async function buyPlan(plan: string) {
-            setIsLoading(true)
-            let planSlug;
-            switch (plan) {
-                case 'basic-annual':
-                    planSlug = 'basicAnnual'
-                    break;
-                case 'business-annual':
-                    planSlug = 'businessAnnual'
-                    break;
-                case 'premium-annual':
-                    planSlug = 'premiumAnnual'
-                    break;
-                default:
-                    planSlug = plan
-                    break;
-            }
-            try {
-                const payload = {
-                    id: userInfo.attributes.sub!,
-                    email: userInfo.attributes.email,
-                    plan_type: planSlug
-                }
-                const options = {
-                    method: 'post',
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(payload)
-                };
-                const response = await fetch(`/api/create-customer`, options);
-                const data = await handleFetchResponse(response)
-                if (data instanceof Error) throw data;
-                setIsLoading(false)
-                //@ts-ignore
-                window.location.href = data.result?.url;
-            } catch (error) {
-                setIsLoading(false)
-                const errorMessage = error instanceof Error ? error.message : 'Something went wrong. We are working on it.'
-                setError(errorMessage)
-            }
-
-        }
-        buyPlan(plan as string)
-    }, [router.query, userInfo.attributes.email, userInfo.attributes.sub])
-
 
     if (router.isFallback) {
         return <PleaseWait />;
@@ -90,14 +34,7 @@ const PlanType = (props: Props) => {
     return (
         <Authenticator components={components}>
             {({ user }) => (
-                <>
-                    {isLoading && <Loading text='Loading checkout details...' />}
-                    {error && <Snackbar open={!!error} autoHideDuration={6000}>
-                        <Alert onClose={() => setError(null)} variant="filled" severity="error" sx={{ width: '100%' }}>
-                            {error}
-                        </Alert>
-                    </Snackbar>}
-                </>
+                <BuyPlan plan={router.query.plan as string} email={user?.attributes?.email!} id={user?.attributes?.sub!} />
             )}
         </Authenticator>
     );
