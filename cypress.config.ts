f339aa2d65b0ac36cd19a82beb049b3e8d28924b
Jessica Yanes <jessica.yanes.90@gmail.com>
2022-12-13 16:38:32 -0500

    Completed the first cypress test with static qr.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     supportFile: false,
+    experimentalStudio:true,
+    viewportWidth: 1024,
+    viewportHeight: 900
   },
 });
--- cypress/e2e/website.cy.ts
+++ cypress/e2e/website.cy.ts
@@ -1,32 +1,33 @@
 describe('Static QR code', () => {
   it('Select Website QR', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000');
 
-    //Finding the span of the button label
-    cy.contains('Static QR Codes').children('span').as('stqr')
-    //Finding the button parent of the span and clicking the button
-    cy.get('@stqr').parent().click()
-    // Finding in the new page the qr type
-    cy.contains('Website').parent().parent().parent().parent().click()
-    cy.get('body').contains('Next').click()
   })
 
-
   it('Complete Content',() =>{
-    cy.get('input[type=text]').should('have.value', '').type('https://server.cenit.io')
-    cy.get('body').contains('Next').click()
-
+    cy.get('.css-1gvpih1-MuiButtonBase-root-MuiTab-root').click();// > .MuiBadge-root > .MuiTypography-root').click();
+    cy.get(':nth-child(2) > .css-b69al4 > .css-1ckupud > .css-cblyn4 > .MuiBox-root > .MuiTypography-root').click();
+    cy.get('body').contains('Next').click();
+    cy.get('#\\:r3\\:').should('have.value', '').type('https://server.cenit.io');
+    cy.get('body').contains('Next').click();
   })
 
   it('Complete QR Desing',() =>{
-    //cy.get('input[value=square]').click()
-    //cy.get('body').contains('Next').click()
-    cy.contains('Squares').parent().parent().parent().click()
-
+    cy.get('body').click();
+    cy.contains('Squares').parent().parent().parent().as('code')
+    cy.get('@code').click()
+    cy.get('[data-value="rounded"] > .MuiListItemText-root > .MuiTypography-root').click();
+    cy.get('body').click();
+    cy.get('.css-a5rdam-MuiGrid-root > :nth-child(3) > .css-oc995m-MuiPaper-root > .css-q7l9ym-MuiFormControl-root').click();
+    cy.get('[data-value="-1"] > .MuiListItemText-root > .MuiTypography-root').click();
+    cy.get('#buttonNext').click();
   })
- /* it('Save and dowload QR',() =>{
-    //cy.get('body').contains('Done')//.click()
-   })*/
+
+  it('Dowload QR',() =>{
+    cy.get('#buttonDow').click();
+    cy.get('.css-xvj9q9-MuiButtonBase-root-MuiButton-root').click();
+    cy.get('#buttonD').click();
+   })
 
 })
 
