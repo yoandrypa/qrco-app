96d2bb78dd511f587ef4e48b106eb2545ef6c4a6
soma <soma@velosimo.com>
2022-12-15 12:38:01 -0500

    these changes are meant to reduce the payload between qrco and microsite

--- components/qr/QrTypeSelector.tsx
+++ components/qr/QrTypeSelector.tsx
@@ -7,11 +7,12 @@ interface QrTypeSelectorProps {
   setSelected: Function;
   selected?: string | null;
   userInfo: string;
+  data: any;
+  options: any;
 }
 
-const QrTypeSelector = () => {
-  // @ts-ignore
-  const {setSelected, selected, userInfo}: QrTypeSelectorProps = useContext(Context);
+const QrTypeSelector = () => { // @ts-ignore
+  const {options, data, setSelected, selected, userInfo}: QrTypeSelectorProps = useContext(Context);
 
   const handleSelect = (payload: string): void => {
     setSelected((prev: string) => prev === payload ? null : payload);
