bf63ae1db7c7e503e789d783c60e3734d6e7d63d
soma <soma@velosimo.com>
2022-12-20 11:25:27 -0500

    updating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,47 @@
-import components from "../libs/aws/components";
-import * as UserHandler from "../handlers/users";
 import * as QrHandler from "../handlers/qrs";
-import QrHome from "../components/qr/QrHome";
-import {
-  GetServerSideProps,
-  GetStaticProps,
-  InferGetServerSidePropsType, InferGetStaticPropsType,
-} from "next";
-import { formFields } from "../libs/aws/components";
-
-import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
+import {GetStaticProps, InferGetStaticPropsType,} from "next";
 import "@aws-amplify/ui-react/styles.css";
-import awsExports from "../libs/aws/aws-exports";
-import { useRouter } from "next/router";
-import PleaseWait from "../components/PleaseWait";
+import {useRouter} from "next/router";
 
 import QrGen from "./qr/type";
-import { useContext, useEffect } from "react";
+import {useContext, useEffect} from "react";
 import Context from "../components/context/Context";
 
 // @ts-ignore
 import session from "@ebanux/ebanux-utils/sessionStorage";
-
-const noUser = "noUser";
+import QrList from "../components/qr/QrList";
+import PleaseWait from "../components/PleaseWait";
 
 export default function Index ({ user }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter(); // @ts-ignore
-  const { clearData } = useContext(Context);
+  const { clearData, setLoading } = useContext(Context);
 
   useEffect(() => {
     clearData();
+
+    if (router.query.login) {
+      setLoading(true);
+      const { path } = router.query;
+      const route = { pathname: path !== undefined ? `${path}` : "/" };
+      if (router.query.selected) { // @ts-ignore
+        route.query = { selected: router.query.selected };
+      }
+      router.push(route, "/", { shallow: false }).then(() => {
+        setLoading(false);
+      });
+    }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  /*if (router.isFallback) {
+  if (router.isFallback) {
     return <PleaseWait/>;
-  }*/
+  }
 
   if (!session.isAuthenticated) {
     return <QrGen/>;
   }
 
   return (
-    <QrHome user={user}/>
+    <QrList user={user}/>
   );
 };
 
