1f22ec17ead581839417f866d92264795c0d32c7
Alain Fernández Deroncere <alainfd82@gmail.com>
2022-12-20 13:18:13 -0500

    - Remove unnecessary functions

--- components/link/LinkHome.tsx
+++ components/link/LinkHome.tsx
@@ -1,31 +1,11 @@
-import { useContext, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LinksTable from "./LinksTable";
 import LinksCreateForm from "./LinksCreateForm";
-import Context from "../context/Context";
-
-import { useRouter } from "next/router";
 
 export default function LinkHome({ linksData, domainsData, userInformation }: any) {
   const { data, total } = JSON.parse(linksData);
   const domains = JSON.parse(domainsData);
-
-  const router = useRouter();
-
-  // @ts-ignore
-  const { userInfo, setUserInfo } = useContext(Context);
-
-  useEffect(() => {
-    if (router.query.login) {
-      const { path } = router.query;
-      // @ts-ignore
-      router.push(Boolean(path) ? path : '/', undefined, { shallow: false });
-    }
-    if (!userInfo) {
-      setUserInfo(userInformation);
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box>
