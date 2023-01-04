4b50f6b3686fd6abfd8466c992a1db785d2009eb
soma <soma@velosimo.com>
2023-01-04 15:48:58 -0500

    removing unused imports and declarations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,16 @@
-import * as QrHandler from "../handlers/qrs";
-import { GetStaticProps, InferGetStaticPropsType, } from "next";
 import "@aws-amplify/ui-react/styles.css";
-import { useRouter } from "next/router";
 
 import QrGen from "./qr/type";
-import { useContext, useEffect } from "react";
+import {useContext, useEffect} from "react";
 import Context from "../components/context/Context";
 
 // @ts-ignore
 import session from "@ebanux/ebanux-utils/sessionStorage";
 import QrList from "../components/qr/QrList";
-import PleaseWait from "../components/PleaseWait";
 
 export default function Index({ user }: any) {
-  const router = useRouter(); // @ts-ignore
-  const { clearData, setLoading } = useContext(Context);
+  // @ts-ignore
+  const { clearData } = useContext(Context);
 
   useEffect(() => {
     clearData();
