5f8cd6c810815066c9a30e6aa45df5909c4127bc
yosle <yosle007@gmail.com>
2023-01-12 08:31:31 -0500

    add usage endpoint

--- pages/api/usage/index.ts
+++ pages/api/usage/index.ts
@@ -1,9 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { recordUsage, reportUsage, getSubscriptionFromCustomer } from '../../../handlers/usage'
 
-type Data = {
-    name: string
-}
+
+
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ name: 'Example' })
+    if (req.method != 'POST') return res.status(405).send('Method not allowed')
+
+    res.status(200).json({ name: 'Subscription usage was successfully setted' })
 }
