493c8249b046c7556cdd10a976f5bd36dd853409
yosle <yosle007@gmail.com>
2023-01-17 06:49:47 -0500

    reduced saveUsage to 2 args

--- components/qr/auxFunctions.ts
+++ components/qr/auxFunctions.ts
@@ -310,7 +310,7 @@ export const saveOrUpdate = async (data: DataType, userInfo: UserInfoProps, opti
         const user = await getUser(userInfo.cognito_user_id);
         if (user.subscriptionData?.status == 'active') {
           try {
-            await saveUsage(user.customerId, user.subscriptionData.id, 1);
+            await saveUsage(user.customerId, 1);
             await recordUsage(1, user.subscriptionData.id);
           } catch (error) {
             console.error('unable to report usage', error)
