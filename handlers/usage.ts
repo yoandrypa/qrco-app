0d1c8379106d6da618046094579ebf1d8afaf578
yosle <yosle007@gmail.com>
2023-01-17 06:48:19 -0500

    refractoring

--- handlers/usage.ts
+++ handlers/usage.ts
@@ -1,25 +1,22 @@
 import Stripe from 'stripe'
-import { findByCustomerId as findUserByCustomerId } from '../handlers/users';
+import { findByCustomerId as findUserByCustomerId, update, get } from '../handlers/users';
 import { getUuid } from '../helpers/qr/helpers';
+
 const stripe = new Stripe(process.env.REACT_STRIPE_SECRET_KEY || 'sk_test_51Ksb3LCHh3XhfaZr2tgzaQKAQtuTF9vRtgdXBS7X2rAaPC6FNoLQ3hyPFVmlnRhsif0FDdbi5cdgEh7Y1Wt9Umo900w9YPUGo6', {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: '2022-08-01',
 });
 
 // Record usage for a metered subscription
-export async function reportUsage(customerId: string, subscriptionId: string, numRequests: number) {
+export async function saveUsage(userId: string, numRequests: number) {
     try {
-        // Get the subscription for this customer
-        const [user] = await findUserByCustomerId(customerId);
-        const subscriptionId = user.subscriptionData.id;
-        const totalUsage = await recordUsage(numRequests, user.id);
-        return ({ numRequests: totalUsage });
+        return await update({ id: userId }, { planUsage: numRequests })
     } catch (e) {
-        if (e instanceof Error) return (new Error(e.message));
+        return e;
     }
 }
 
-export async function recordUsage(usageQuantity: number, subscriptionId: string) {
+export async function recordPlanUsage(usageQuantity: number, subscriptionId: string, customerId: string) {
     const subscriptionItems = await stripe.subscriptionItems.list({
         subscription: subscriptionId,
     });
@@ -52,7 +49,3 @@ export async function recordUsage(usageQuantity: number, subscriptionId: string)
 
 
 
-
-export async function getSubscriptionFromCustomer(customerId: string) {
-
-}
