6383ab27535543931beb47280b51a6fc68410429
yoandrypa <yoandrypa@gmail.com>
2023-02-05 21:46:51 -0500

    Update pre-generate service

--- scripts/pre-codes/index.js
+++ scripts/pre-codes/index.js
@@ -2,13 +2,13 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .name('pre-generate')
-  .description('QR Generator tools')
+  .name('pre-codes')
+  .description('QR-Codes tools')
   .version('1.0.0')
 
 program
-  .command('pre-codes')
-  .description('Allow pre-generate QR-Codes that will be available to be claimed.')
+  .command('generate')
+  .description('Allow generate Pre-QR-Codes that will be available to be claimed.')
   .requiredOption('-u, --base-url,  [baseUrl]', 'Set QRApp server base URL', 'http://127.0.0.1:3000')
   .requiredOption('-s, --size,      [size]', 'Set length of QR-Link code', '8')
   .requiredOption('-c, --count,     [count]', 'Set the number of codes that will be generated', '10')
@@ -18,6 +18,22 @@ program
     const headers = { 'Content-type': 'application/json; charset=UTF-8' };
 
     axios.post(`${baseUrl}/api/pre-generate`, data, { headers }).then((response) => {
+      console.log(response.data);
+    }).catch((err, response) => {
+      console.error(err.message);
+      err.response?.data && console.error(err.response.data);
+    })
+  });
+
+program
+  .command('list')
+  .description('Allow list the Pre-QR-Codes that will be available to be claimed.')
+  .requiredOption('-u, --base-url,  [baseUrl]', 'Set QRApp server base URL', 'http://127.0.0.1:3000')
+  .requiredOption('-o, --owner,     [owner]', 'Set the owner of codes', 'any')
+  .action(({ baseUrl, owner }) => {
+    const axios = require('axios');
+
+    axios.get(`${baseUrl}/api/pre-generate?owner=${owner}`).then((response) => {
       console.log(response.data);
     }).catch((err, response) => {
       console.error(err.message);
