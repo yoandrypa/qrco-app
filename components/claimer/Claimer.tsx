b0a24a317100020cdae3fcdd4d93e1e43ae7cc49
yoandrypa <yoandrypa@gmail.com>
2023-02-20 10:06:08 -0500

    Fix claimer page

--- components/claimer/Claimer.tsx
+++ components/claimer/Claimer.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, KeyboardEvent, MouseEvent, useEffect, useRef, useState} from "react";
+import React, { ChangeEvent, KeyboardEvent, MouseEvent, useEffect, useRef, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
@@ -6,16 +6,17 @@ import Popover from "@mui/material/Popover";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import {styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 
-import {IS_DEV_ENV, MAIN_ORANGE} from "../qr/constants";
+import { IS_DEV_ENV, MAIN_ORANGE } from "../qr/constants";
 import RenderPreview from "../qr/renderers/RenderPreview";
 import RenderDownloadPrint from "../qr/helperComponents/looseComps/RenderDownloadPrint";
 
-import {useRouter} from "next/router";
-import {findByAddress} from "../../handlers/links";
+import { useRouter } from "next/router";
+import { findByAddress } from "../../handlers/links";
+import { iFrameDetected } from "@ebanux/ebanux-utils/utils";
 
-const Typo = styled(Typography)(({bold}: {bold?: boolean}) => ({
+const Typo = styled(Typography)(({ bold }: { bold?: boolean }) => ({
   display: 'inline',
   fontWeight: bold ? 'bold' : 'unset',
   fontSize: '20px'
@@ -25,10 +26,9 @@ const URL = 'https://a-qr.link/';
 
 interface ClaimerProps {
   code: string;
-  embedded?: boolean;
 }
 
-export default function Claimer({code, embedded}: ClaimerProps) {
+export default function Claimer({ code }: ClaimerProps) {
   const [custom, setCustom] = useState<string>(code || '');
   const [open, setOpen] = useState<HTMLButtonElement | null>(null);
   const [available, setAvailable] = useState<boolean>(true);
@@ -43,13 +43,13 @@ export default function Claimer({code, embedded}: ClaimerProps) {
   }
 
   const handleCustom = (event: ChangeEvent<HTMLInputElement>) => {
-    const {value}: {value: string} = event.target;
+    const { value }: { value: string } = event.target;
     lynk.current = `${URL}${value}`;
     setCustom(value);
   }
 
   const handleClaim = (event: MouseEvent<HTMLButtonElement>) => {
-    if (!embedded) {
+    if (!iFrameDetected) {
       setOpen(event.currentTarget);
     } else if (window && window.top) {
       window.top.location.href = `${window.location.origin}/qr/type?address=${custom}`;
@@ -63,7 +63,7 @@ export default function Claimer({code, embedded}: ClaimerProps) {
     const checkData = setTimeout(async () => { //this implements a debounce for checking the availability
       if (!isError) {
         setChecking(true);
-        const links = await findByAddress({address: {eq: custom}});
+        const links = await findByAddress({ address: { eq: custom } });
         setAvailable(links.length === 0);
         setChecking(false);
       }
@@ -78,17 +78,18 @@ export default function Claimer({code, embedded}: ClaimerProps) {
       left: "50%",
       transform: "translate(-50%, -50%)"
     }}>
-      <Paper sx={{ p: 2, mx: 'auto', width: !embedded ? {sm: '400px', xs: '100%'} : '350px' }} elevation={!embedded ? 3 : 0}>
-        <Box sx={{mb: 2, width: '100%', textAlign: 'center'}}>
+      <Paper sx={{ p: 2, mx: 'auto', width: !embedded ? { sm: '400px', xs: '100%' } : '350px' }}
+             elevation={!embedded ? 3 : 0}>
+        <Box sx={{ mb: 2, width: '100%', textAlign: 'center' }}>
           <Typo bold>QR</Typo>
-          <Typo sx={{color: MAIN_ORANGE}} bold>Lynk</Typo>
+          <Typo sx={{ color: MAIN_ORANGE }} bold>Lynk</Typo>
         </Box>
         <Paper elevation={2}>
           <RenderPreview override={lynk.current} width="100%" onlyPreview />
         </Paper>
 
-        <Box sx={{mt: '10px'}}>
-          <Box sx={{display: 'flex', mt: '-5px', height: !embedded ? '85px' : 'unset'}}>
+        <Box sx={{ mt: '10px' }}>
+          <Box sx={{ display: 'flex', mt: '-5px', height: !embedded ? '85px' : 'unset' }}>
             <TextField
               onKeyDown={(evt: KeyboardEvent<HTMLInputElement>) => !/^[a-zA-Z0-9_]+$/.test(evt.key) && evt.preventDefault()}
               label=""
@@ -99,20 +100,20 @@ export default function Claimer({code, embedded}: ClaimerProps) {
               value={custom}
               error={isError || !available}
               helperText={isError ? 'Make sure you entered a code' : (available ? '' : 'The entered code is already taken')}
-              sx={{'& fieldset': {borderRadius: '5px 0 0 5px'}}}
+              sx={{ '& fieldset': { borderRadius: '5px 0 0 5px' } }}
               onChange={handleCustom}
               InputProps={{
                 startAdornment: (
-                  <InputAdornment position="start" sx={{mr: 0}}>
-                    <Typography sx={{mt: '2px'}}>{URL.slice(8)}</Typography>
+                  <InputAdornment position="start" sx={{ mr: 0 }}>
+                    <Typography sx={{ mt: '2px' }}>{URL.slice(8)}</Typography>
                   </InputAdornment>
                 )
-              }}/>
+              }} />
             <Button
               onClick={handleClaim}
-              variant="outlined" sx={{height: '40px', mt: '8px', borderRadius: '0 5px 5px 0'}}
+              variant="outlined" sx={{ height: '40px', mt: '8px', borderRadius: '0 5px 5px 0' }}
               disabled={isError || !available || checking}>
-              <Typography sx={{mx: '5px'}}>{'Claim'}</Typography>
+              <Typography sx={{ mx: '5px' }}>{'Claim'}</Typography>
             </Button>
           </Box>
         </Box>
@@ -122,11 +123,12 @@ export default function Claimer({code, embedded}: ClaimerProps) {
           open
           anchorEl={open}
           onClose={() => setOpen(null)}
-          anchorOrigin={{vertical: 'top', horizontal: 'center'}}
-          transformOrigin={{vertical: 'top', horizontal: 'center'}}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
         >
-          <Box sx={{p: 2}}> {/* @ts-ignore */}
-            <RenderDownloadPrint qrImageData={`${document.getElementById('qrCodeReferenceId').outerHTML}`} code={custom} />
+          <Box sx={{ p: 2 }}> {/* @ts-ignore */}
+            <RenderDownloadPrint qrImageData={`${document.getElementById('qrCodeReferenceId').outerHTML}`}
+                                 code={custom} />
           </Box>
         </Popover>
       )}
