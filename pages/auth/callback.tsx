115ac15ec6c1a6004a53d7fb52e03289a7db8875
yoandrypa <yoandrypa@gmail.com>
2023-02-22 09:26:26 -0500

    Fix auth-flow

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import session from "@ebanux/ebanux-utils/sessionStorage";
-import { Authenticator, authWithAuthCode } from "@ebanux/ebanux-utils/auth";
+import { Authenticator } from "@ebanux/ebanux-utils/auth";
 
 import Alert from "@mui/material/Alert";
 
@@ -13,35 +13,22 @@ export default function AuthCallback() {
 
   useEffect(() => {
     startWaiting();
-    console.log(222, session.oauthRedirectUri, session.isAuthenticating, session.isAuthenticated);
-
     if (session.isAuthenticated) {
       const callbackRoute = session.get('CALLBACK_ROUTE', '/', true);
 
       router.push(callbackRoute, callbackRoute.pathname || '/').finally(() => {
         releaseWaiting();
       });
-    } else if (session.isAuthenticating) {
-      console.log(111, router.query.code, session.oauthRedirectUri);
-      const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
-      const authCode: string | null = urlParams.get('code');
-      authWithAuthCode(authCode as string).then(() => {
-        console.log(222, session.oauthRedirectUri, session.isAuthenticated);
-        // window.location.replace(session.oauthRedirectUri)
-      }).catch((err: any) => {
-        console.error(err.message);
-      });
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <Alert severity="success" variant="outlined">Welcome</Alert>
-  // return (
-  //   <Authenticator>
-  //     {({ user }: any) => (
-  //       <Alert severity="success" variant="outlined">
-  //         Welcome: {user.name || user.email}.
-  //       </Alert>
-  //     )}
-  //   </Authenticator>
-  // );
+  return (
+    <Authenticator>
+      {({ user }: any) => (
+        <Alert severity="success" variant="outlined">
+          Welcome: {user.name || user.email}.
+        </Alert>
+      )}
+    </Authenticator>
+  );
 };
