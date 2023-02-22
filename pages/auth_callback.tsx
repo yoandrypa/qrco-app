055100ca4d0f7287a77d0bec6574a0e98290655f
yoandrypa <yoandrypa@gmail.com>
2023-02-22 02:50:28 -0500

    Fix auth-flow

--- pages/auth_callback.tsx
+++ pages/auth_callback.tsx
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import session from "@ebanux/ebanux-utils/sessionStorage";
-// import { Authenticator } from "@ebanux/ebanux-utils/auth";
-import { Authenticator } from "./auth";
+import { Authenticator } from "@ebanux/ebanux-utils/auth";
 
 import Alert from "@mui/material/Alert";
 
