b88f1c25e2fb7842a1f8ce12f07cdaa209135887
yoandrypa <yoandrypa@gmail.com>
2023-02-22 03:19:00 -0500

    Fix auth-flow

--- pages/auth_callback.tsx
+++ pages/auth_callback.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import session from "@ebanux/ebanux-utils/sessionStorage";
-import { Authenticator } from "@ebanux/ebanux-utils/auth";
+import { Authenticator, authWithAuthCode } from "@ebanux/ebanux-utils/auth";
 
 import Alert from "@mui/material/Alert";
 
@@ -18,6 +18,16 @@ export default function AuthCallback() {
 
       router.push(callbackRoute, callbackRoute.pathname || '/').finally(() => {
         releaseWaiting();
+      });
+    } else if (session.isAuthenticating) {
+      console.log(111, router.query.code, session.oauthRedirectUri);
+      const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+      const authCode: string | null = urlParams.get('code');
+      authWithAuthCode(authCode as string).then(() => {
+        console.log(222, session.oauthRedirectUri, session.isAuthenticated);
+        // window.location.replace(session.oauthRedirectUri)
+      }).catch((err: any) => {
+        console.error(err.message);
       });
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
