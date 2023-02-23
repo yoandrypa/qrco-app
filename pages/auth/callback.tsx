b7bce2620a1a704c9afacc475d892566147881e6
yoandrypa <yoandrypa@gmail.com>
2023-02-23 00:44:34 -0500

    Fix auth-flow

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
 import session from "@ebanux/ebanux-utils/sessionStorage";
-import { Authenticator } from "@ebanux/ebanux-utils/auth";
+import { Authenticator, authWithAuthCode } from "@ebanux/ebanux-utils/auth";
 
 import Alert from "@mui/material/Alert";
 
@@ -10,25 +10,30 @@ import { startWaiting, releaseWaiting } from "../../components/Waiting";
 
 export default function AuthCallback() {
   const router = useRouter();
+  const { isAuthenticating, isAuthenticated, currentUser } = session;
 
   useEffect(() => {
     startWaiting();
-    if (session.isAuthenticated) {
-      const callbackRoute = session.get('CALLBACK_ROUTE', '/', true);
 
-      router.push(callbackRoute, callbackRoute.pathname || '/').finally(() => {
-        releaseWaiting();
+    const callbackRoute = session.get('CALLBACK_ROUTE', '/');
+
+    if (isAuthenticating) {
+      const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+      const authCode: string | null = urlParams.get('code');
+
+      authWithAuthCode(authCode as string).then(() => {
+        router.push(callbackRoute, callbackRoute.pathname || '/').finally(() => releaseWaiting());
+      }).catch((err: any) => {
+        console.error(err.message);
       });
+    } else if (isAuthenticated) {
+      router.push(callbackRoute, callbackRoute.pathname || '/').finally(() => releaseWaiting());
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <Authenticator>
-      {({ user }: any) => (
-        <Alert severity="success" variant="outlined">
-          Welcome: {user.name || user.email}.
-        </Alert>
-      )}
-    </Authenticator>
+  if (isAuthenticated) return (
+    <Alert severity="success" variant="outlined"> Welcome: {currentUser.name || currentUser.email}.</Alert>
   );
+
+  return <div />
 };
