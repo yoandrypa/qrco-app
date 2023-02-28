ee9d800b668f3f6cc89fb121408ab486148bd60a
yoandrypa <yoandrypa@gmail.com>
2023-02-28 01:59:59 -0500

    Update plans features

--- components/plans/PlanCard.tsx
+++ components/plans/PlanCard.tsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import Done from '@mui/icons-material/Done'
+import AllowIcon from '@mui/icons-material/Done'
+import NoAllowIcon from '@mui/icons-material/CloseOutlined'
 import Paper from '@mui/material/Paper'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Context from "../context/Context";
@@ -9,11 +10,11 @@ import Context from "../context/Context";
 interface CardOptions {
   title: string,
   description: string,
-  plan_type: PlanType,
+  planType: PlanType,
   priceAmount: string,
   legend: string,
   buttonText: string,
-  features: string[],
+  features: any,
   highlighted?: boolean
 }
 
@@ -23,15 +24,72 @@ type PlanCardProps = {
   isCurrentPlan: boolean
 }
 
+type FeatureProps = {
+  name: string;
+  value: number | boolean | string;
+}
+
+const parseUpToText = (value: number | boolean | string, feature: string) => {
+  if (value === -1) return `Unlimited ${feature}`
+  if (value === 0) return `No ${feature} are allowed`;
+  return `Up to ${value} ${feature}`;
+}
+
+const parseAmountText = (value: number | boolean | string, feature: string) => {
+  if (value === -1) return `Unlimited ${feature}`
+  if (value === 0) return `No ${feature} are allowed`;
+  return `${value} per ${feature}`;
+}
+
+const parseFeatureText = (feature: string, value: number | boolean | string) => {
+  switch (feature) {
+    case 'upToDynamicQR':
+      return parseUpToText(value, 'dynamic QR code');
+    case 'amountByAdditionalDynamicQR':
+      return parseAmountText(value, 'additional dynamic QR');
+    case 'upToPreGeneratedQR':
+      return parseUpToText(value, 'pre-generated QRs');
+    case 'upToMicroSite':
+      return parseUpToText(value, 'micro-site (mobile-friendly landing page');
+    case 'upToStaticQR':
+      return parseUpToText(value, 'static QR codes');
+    case 'upToScans':
+      return parseUpToText(value, 'scans');
+    case 'allowQRCodesDesign':
+      return 'QR codes design customization and edition';
+    case 'allowEditDynamicQRContent':
+      return 'Dynamic QR codes content edition';
+    case 'allowEditMicroSite':
+      return 'Micro-sites appearance customization and edition';
+  }
+}
+
+const Feature = ({ name, value }: FeatureProps) => {
+  const icon = !!value ? <AllowIcon color='success' /> : <NoAllowIcon color='error' />;
+
+  return (
+    <Grid container sx={{ minHeight: 60 }}>
+      <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'right', paddingRight: 1 }}>
+        {icon}
+      </Grid>
+      <Grid item xs={10}>
+        <Typography sx={{ paddingRight: 1 }}>
+          {parseFeatureText(name, value)}
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
 const PlanCard = ({ data, clickAction, isCurrentPlan }: PlanCardProps) => {
   // @ts-ignore
   const { subscription } = useContext(Context);
-  const { title, description, plan_type, legend, priceAmount, highlighted, buttonText, features } = data;
+  const { title, description, planType, legend, priceAmount, highlighted, buttonText, features } = data;
 
   const elevation = isCurrentPlan ? 5 : 2;
   const bgColor = isCurrentPlan ? '#efffef' : '#ffffff';
   const disabled = !clickAction
-    || plan_type === 'free'
+    || planType === 'free'
     || (!isCurrentPlan && subscription && subscription.status !== 'canceled');
 
   return (
@@ -65,26 +123,22 @@ const PlanCard = ({ data, clickAction, isCurrentPlan }: PlanCardProps) => {
         <Typography color='gray' textAlign={'center'}> {legend}</Typography>
         <Grid sx={{ justifyContent: 'center', alignContent: 'center', display: 'flex', margin: 2 }}>
           <LoadingButton disabled={disabled}
-                         onClick={() => clickAction?.(plan_type)}
+                         onClick={() => clickAction?.(planType)}
                          variant={highlighted ? 'contained' : 'outlined'}>
             {isCurrentPlan ? 'Review' : buttonText || 'Buy now'}
           </LoadingButton>
         </Grid>
-        {
-          features?.map((feature, index) => (
-              <Grid key={index} container sx={{ minHeight: 60 }}>
-                <Grid item key={index} xs={2} sx={{ display: 'flex', justifyContent: 'right', paddingRight: 1 }}>
-                  <Done color='success' />
-                </Grid>
-                <Grid item xs={10}>
-                  <Typography sx={{ paddingRight: 1 }}>
-                    {feature}
-                  </Typography>
-                </Grid>
-              </Grid>
-            )
-          )
-        }
+
+        <Feature name="upToDynamicQR" value={features.upToDynamicQR} />
+        <Feature name="amountByAdditionalDynamicQR" value={features.amountByAdditionalDynamicQR} />
+        <Feature name="upToPreGeneratedQR" value={features.upToPreGeneratedQR} />
+        <Feature name="upToMicroSite" value={features.upToMicroSite} />
+        <Feature name="upToStaticQR" value={features.upToStaticQR} />
+        <Feature name="upToScans" value={features.upToScans} />
+        <Feature name="allowQRCodesDesign" value={features.allowQRCodesDesign} />
+        <Feature name="allowEditDynamicQRContent" value={features.allowEditDynamicQRContent} />
+        <Feature name="allowEditMicroSite" value={features.allowEditMicroSite} />
+
       </Paper>
     </Grid>
   )
