263955b836f93bc31bb640b6acd376ed4ec7e72a
yoandrypa <yoandrypa@gmail.com>
2023-03-09 14:40:49 -0500

    Fix login callback

--- components/menus/MainMenu/ButtonLogin.tsx
+++ components/menus/MainMenu/ButtonLogin.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
 
+import session from "@ebanux/ebanux-utils/sessionStorage";
+
+import Button from "@mui/material/Button";
 import LoginIcon from "@mui/icons-material/Login";
 
 import classes from "./classes.sx";
-import Button from "@mui/material/Button";
 
-import { onLogin } from "./MenuItemLogin";
+import { startWaiting } from "../../Waiting";
+import { startAuthorizationFlow } from "@ebanux/ebanux-utils/auth";
 
 export default function ButtonLogin() {
+  const router = useRouter();
   const { navButton } = classes;
+
+  const onLogin = () => {
+    startWaiting();
+    const { pathname, query } = router;
+    session.set('CALLBACK_ROUTE', { pathname, query });
+    startAuthorizationFlow();
+  }
 
   return (
     <Button variant="contained"
--- components/menus/MainMenu/MenuItemLogin.tsx
+++ components/menus/MainMenu/MenuItemLogin.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import { useRouter } from "next/router";
+
+import session from "@ebanux/ebanux-utils/sessionStorage";
 
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-
 import LogoutIcon from "@mui/icons-material/Logout";
 
 import classes from "./classes.sx";
+
 import { startAuthorizationFlow } from "@ebanux/ebanux-utils/auth";
 import { startWaiting } from "../../Waiting";
 
-export const onLogin = () => {
-  startWaiting();
-  startAuthorizationFlow();
-}
-
 export default function MenuItemLogin() {
+  const router = useRouter();
   const { iconSmall } = classes;
+
+  const onLogin = () => {
+    startWaiting();
+    const { pathname, query } = router;
+    session.set('CALLBACK_ROUTE', { pathname, query });
+    startAuthorizationFlow();
+  }
 
   return (
     <MenuItem onClick={onLogin}>
