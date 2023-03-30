573fe994c9a931f1f1669fc36cf05c3e8f41517f
yoandrypa <yoandrypa@gmail.com>
2023-03-30 13:31:31 -0400

    Add login endpoint

---
+++ pages/login/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import session from "@ebanux/ebanux-utils/sessionStorage";
+
+import { useRouter } from "next/router";
+import { startAuthorizationFlow } from "../../libs/utils/auth";
+
+import { startWaiting, releaseWaiting } from "../../components/Waiting";
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    startWaiting();
+    if (session.isAuthenticated) {
+      router.push('/').finally(releaseWaiting);
+    } else {
+      startAuthorizationFlow({ pathname: '/' });
+    }
+  }, []);  // eslint-disable-line react-hooks/exhaustive-deps
+};
