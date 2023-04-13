5e7a75407a652c6cd0659f523a8fbe5fd464530c
soma <soma@velosimo.com>
2023-04-13 13:28:57 -0400

    adding edition capabilities

--- components/qr/helperComponents/smallpieces/RenderLinkItem.tsx
+++ components/qr/helperComponents/smallpieces/RenderLinkItem.tsx
@@ -1,15 +1,24 @@
 import {MouseEvent, useCallback, useState} from "react";
 import LinkIcon from "@mui/icons-material/Link";
+import Box from "@mui/material/Box";
+import EditIcon from '@mui/icons-material/Edit';
+
+import RenderIcon from "./RenderIcon";
 
 import dynamic from "next/dynamic";
+import IconButton from "@mui/material/IconButton";
+
 const EditCustomLinkCode = dynamic(() => import("./EditCustomLinkCode"));
 
 interface LinkItemProps {
   step: number;
   code: string;
+  selected: string;
+  onlyQr: boolean;
+  urlData: string;
 }
 
-export default function RenderLinkItem({step, code}: LinkItemProps) {
+export default function RenderLinkItem({step, code, onlyQr, selected, urlData}: LinkItemProps) {
   const [anchor, setAnchor] = useState<undefined | HTMLElement>(undefined);
 
   const handleEdit = useCallback((event: MouseEvent<HTMLElement>) => {
@@ -24,10 +33,35 @@ export default function RenderLinkItem({step, code}: LinkItemProps) {
     }} />
   ), []);
 
+  const renderUrlData = () => {
+    const index = urlData.lastIndexOf('/') + 1;
+    return (
+      <Box sx={{display: 'flex', width: !onlyQr ? '190px' : '200px', justifyContent: 'space-between'}}>
+        <Box sx={{
+          mt: '14px', ml: !onlyQr ? '10px' : 0, fontSize: '13px',
+          overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', direction: 'rtl', textAlign: 'left'
+        }}>
+          <span>{!onlyQr ? urlData.slice(0, index) : urlData}</span>
+          {!onlyQr && <span style={{fontWeight: 'bold'}}>{urlData.slice(index)}</span>}
+        </Box>
+        {!onlyQr && step === 1 && (
+          <IconButton size="small" sx={{height: '28px', width: '28px', mt: '9px', mr: '-5px'}} onClick={handleEdit}>
+            <EditIcon fontSize="small"/>
+          </IconButton>
+        )}
+      </Box>
+    );
+  }
+
   return (
-    <>
-      {step === 1 ? <div onClick={handleEdit}>{renderIcon(true)}</div> : renderIcon(false)}
-      {anchor && <EditCustomLinkCode setAnchor={setAnchor} anchor={anchor} current={code} />}
-    </>
+    <Box sx={{ display: 'flex', width: !onlyQr ? '208px' : '231px' }}>
+      {!onlyQr ? (
+        <>
+          {step === 1 ? <div onClick={handleEdit}>{renderIcon(true)}</div> : renderIcon(false)}
+          {anchor && <EditCustomLinkCode setAnchor={setAnchor} anchor={anchor} current={code}/>}
+        </>
+      ) : <Box sx={{mt: '12px', mr: '5px'}}><RenderIcon icon={selected || ''} enabled/></Box>}
+      {renderUrlData()}
+    </Box>
   );
 }
