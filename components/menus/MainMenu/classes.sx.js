1c79faec32415f0bd09b056d8ac8ba50c1d1a02b
yoandrypa <yoandrypa@gmail.com>
2023-05-02 03:19:04 -0400

    Add managements options

--- components/menus/MainMenu/classes.sx.js
+++ components/menus/MainMenu/classes.sx.js
@@ -1,16 +1,7 @@
-const styles = {
-  iconSmall: {
-    width: 22,
-    height: 22,
-    color: 'primary.main'
-  },
+import baseStyles from '../commons/classes.sx'
 
-  navButton: {
-    height: 28,
-    my: 'auto',
-    mr: '5px',
-    textTransform: 'none',
-  },
+const styles = {
+  ...baseStyles,
 
   profile: {
     backgroundColor: 'primary.light',
@@ -29,30 +20,6 @@ const styles = {
       mr: 4
     }
   },
-
-  paperSx: {
-    overflow: 'visible',
-    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-    mt: 1.5,
-    '& .MuiAvatar-root': {
-      width: 32,
-      height: 32,
-      ml: -0.5,
-      mr: 1,
-    },
-    '&:before': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      top: 0,
-      right: 14,
-      width: 10,
-      height: 10,
-      bgcolor: 'background.paper',
-      transform: 'translateY(-50%) rotate(45deg)',
-      zIndex: 0,
-    },
-  }
 };
 
 export default styles;
---
+++ helpers/managements/index.ts
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from 'react';
+import { payLynkRequest } from "../../libs/utils/request";
+
+function onRequest(data: any, appWindow: Window) {
+  const { options, requestId } = data;
+  payLynkRequest({ ...options, inBackground: true }).then((response: any) => {
+    appWindow.postMessage({ cmd: 'response', response, requestId }, '*');
+  }).catch((error: any) => {
+    appWindow.postMessage({ cmd: 'error', error, requestId }, '*');
+  });
+}
+
+export function onMessage(event: any) {
+  const { origin, source: { window: appWindow }, data } = event;
+  const { cmd } = data;
+
+  if (origin !== process.env.PAYLINK_APP_URL) return false;
+  if (cmd === 'doRequest') return onRequest(data, appWindow);
+}
+
+export function startUpEventListener() {
+  // Anything in here is fired on component mount.
+  window.addEventListener("message", onMessage);
+  return () => {
+    // Anything in here is fired on component unmount.
+    window.removeEventListener("message", onMessage);
+  }
+}
+
+export function useDidUpdated() {
+  const didUpdatedRef = useRef(false);
+
+  useEffect(() => {
+    didUpdatedRef.current = true;
+  }, []);
+
+  return didUpdatedRef.current;
+};
